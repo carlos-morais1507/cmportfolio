c7930015dcaae390400eb558ed5db5499ead0e14
c.morais <c.morais1507@gmail.com>
2022-12-12 12:37:47 -0300

    Bugfix with import on deploy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection';
-import AboutSection from '../components/aboutSection';
+import AboutSection from '../components/AboutSection';
 import { aboutData, contactData, skillsData, workData } from '../store/Data';
 import SkillsSection from '../components/SkillsSection';
 import WorkSection from '../components/WorkSection';
